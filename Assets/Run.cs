f8ab2196e69888b828ed27fe1487536df011cf50
Susi <braeden8.susi6@gmail.com>
2024-11-24 23:33:43 -0500

    whoo

--- Assets/Run.cs
+++ Assets/Run.cs
@@ -1,22 +1,115 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
+using System.Globalization;
 using UnityEngine;
+using System;
+using UnityEngine.Networking;
+using static System.Net.WebRequestMethods;
 
 public class Run : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public void UpdateLight(string city)
+    //Weather
+    private DateTime timeCurrent = new DateTime();
+    private float secondsNow;
+    private XmlDocument doc;
+    private string LightMapAccessor;
+
+    //Images
+    private int index;
+    private Texture2D[] tex2D = { null, null, null };
+    [SerializeField] private GameObject billboard;
+    private readonly string[] texWWW = {"https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/2560px-Cat_August_2010-4.jpg",
+                                        "https://upload.wikimedia.org/wikipedia/commons/c/cf/Curious_Raccoon.jpg",
+                                        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Mam%C3%B5es.jpg/1280px-Mam%C3%B5es.jpg"};
+
+    private void Start()
     {
-        StartCoroutine(WeatherManager.GetWeatherXML(OnXMLDataLoaded, city));
-    }
-    public void OnXMLDataLoaded(string data)
-    {
-        Debug.Log(data);
+        timeCurrent = DateTime.Now;
     }
 
-    // Update is called once per frame
-    void Update()
+    //Weather
+    public void UpdateCity(string city)
     {
-        
+        LightMapAccessor = "";
+        StartCoroutine(WeatherManager.GetWeatherXML(OnXMLDataLoaded, city));
+    }
+    public void OnXMLDataLoaded(XmlDocument data)
+    {
+        Debug.Log("Loading");
+        doc = data;
+        UpdateWeather();
+        UpdateTime();
+        LoadMap();
+    }
+
+    private void UpdateWeather()
+    {
+        if (true)
+        {
+            LightMapAccessor += "Cloudy";
+        }
+    }
+
+    private void UpdateTime()
+    {
+        Debug.Log("updating");
+        DateTime now = DateTime.UtcNow;
+        Debug.Log(now.ToString());
+
+        XmlNode node = doc.SelectSingleNode("//city/timezone");
+        int secondUpdate = Convert.ToInt32(node.InnerText);
+        if (now.AddSeconds(secondUpdate).Hour >= 19)
+        {
+            LightMapAccessor += "Night";
+        }
+        else
+        {
+            LightMapAccessor += "Day";
+        }
+    }
+
+    private void LoadMap()
+    {
+        Debug.Log(LightMapAccessor);
+        RenderSettings.skybox = Resources.Load<Material>("SkyMaps/" + LightMapAccessor);
+    }
+
+    //Images
+
+
+    public void BillboardButton()
+    {
+        DownloadImage(SetImage, "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/2560px-Cat_August_2010-4.jpg");
+        Debug.Log("Calling");
+    }
+
+    public void SetImage(Texture2D tex)
+    {
+        billboard.GetComponent<Renderer>().material.SetTexture(tex.name, tex);
+        Debug.Log("trying to set");
+    }
+    public void GetWebImage(Action<Texture2D> callback)
+    {
+        index++;
+        index %= texWWW.Length;
+        if (tex2D[index] == null)
+        {
+            Debug.Log(texWWW[index]);
+            StartCoroutine(DownloadImage(SetImage, texWWW[index]));
+        } else
+        {
+            Debug.Log(tex2D[index].name);
+            callback(tex2D[index]);
+        }
+    }
+    public IEnumerator DownloadImage(Action<Texture2D> callback, string www)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(www);
+        Debug.Log("Request object made");
+        yield return request.SendWebRequest();
+        Debug.Log("Request returned");
+        //tex2D[index] = DownloadHandlerTexture.GetContent(request);
+        callback(DownloadHandlerTexture.GetContent(request));
     }
 }
--- Assets/WeatherManager.cs
+++ Assets/WeatherManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Xml;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,10 @@ using UnityEngine.Networking;
 public static class WeatherManager
 {
 
-    private const string xmlApifirsthalf = "http://api.openweathermap.org/data/2.5/weather?q=";
+    private const string xmlApifirsthalf = "http://api.openweathermap.org/data/2.5/weather?mode=xml&q=";
     private const string xmlApisechalf = "&appid=a202d7af555b33476ce91905e43dd07c";
 
-    private static IEnumerator CallAPI(string url, Action<string> callback)
+    private static IEnumerator CallAPI(string url, Action<XmlDocument> callback)
     {
         using (UnityWebRequest request = UnityWebRequest.Get(url))
         {
@@ -25,12 +26,14 @@ public static class WeatherManager
             }
             else
             {
-                callback(request.downloadHandler.text);
+                XmlDocument xml = new XmlDocument();
+                xml.LoadXml(request.downloadHandler.text);
+                callback(xml);
             }
         }
     }
 
-    public static IEnumerator GetWeatherXML(Action<string> callback, string city)
+    public static IEnumerator GetWeatherXML(Action<XmlDocument> callback, string city)
     {
         string xmlApi = xmlApifirsthalf + city + xmlApisechalf; 
         return CallAPI(xmlApi, callback);
